6c937e01457d1f271e39ddbfd160b8c5f8613454
William Almeida Lima <x381192@rede.sp>
2025-06-04 13:51:17 -0300

    feat: new gradient and mousemove

--- src/components/MouseFollower.vue
+++ src/components/MouseFollower.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="ball" ref="ball">
+    
 
     </div>
+
 </template>
 
 <script setup>
@@ -14,7 +16,7 @@ const moveball = (event)=>{
     gsap.to(ball.value,{
         x: event.clientX,
         y: event.clientY,
-        duration: 0.9,
+        duration: 1.5,
         ease: "power2.out"
     });
 }
@@ -44,7 +46,8 @@ onUnmounted(() => {
   left: -20px;
   pointer-events: none;
   z-index: 9999;
-  mix-blend-mode: hard-light;
+  mix-blend-mode: normal;
+  background: linear-gradient(45deg, #42444441, #ffffff6e);
 }
 
 </style>
--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -7,21 +7,28 @@
   <div class="w-full flex justify-center items-center">
     <img src="./photos/profile-animated.png" alt="" class="w-[400px] h-[600px] rounded-full" />
     <div
-      class="bg-white rounded-lg p-4 mt-4 w-[40%] flex flex-col text-3xl text-gray-600 relative left-1 text-center"
+      class="h-[400px] rounded-lg p-1 mt-4 w-[60%] overflow-hidden relative text-3xl text-gray-600 left-1 text-center"
     >
-      <p>Olá tudo bem, me chamo William tenho 28 anos</p>
-      <p>
-        Sou um desenvolvedor Front-End e Web Designer, atualmente estou estudando Vue.js e Tailwind
-        CSS
-      </p>
-      <p>
-        Estou sempre em busca de novos desafios e aprendizados, gosto de trabalhar em equipe e
-        compartilhar conhecimento
-      </p>
+      <div
+        class="gradient-circle text-3xl text-gray-600 absolute"
+      ></div>
+
+      <div class="moveOn h-full m-auto p-4 bg-white rounded flex flex-col gap-2 justify-center">
+        <p><strong>Oi, tudo bem?</strong></p>
+        <p>Me chamo William Almeida Lima e tenho 28 anos!</p>
+        <p>
+          Atualmente, estou me aprofundando em Vue.js e Tailwind CSS sempre buscando evoluir
+          tecnicamente e encarar novos desafios.
+        </p>
+        <p>
+          Sou apaixonado por design e tecnologia, gosto de trabalhar em equipe e acredito no poder
+          da troca de conhecimento para crescer junto.
+        </p>
+      </div>
     </div>
   </div>
 
-  <div class="w-full flex justify-center items-center mt-[100px]">
+  <div class="w-full flex justify-center items-center mt-[100px] moveOn">
     <ul class="text-2xl font-bold flex gap-4 text-gray-600 cursor-pointer">
       <li>📝Ver Portifólio</li>
       <li>📞Fale Comigo</li>
@@ -29,7 +36,7 @@
     </ul>
   </div>
 
-  <div class="px-[200px] mt-24">
+  <div class="px-[200px] mt-24 moveOn">
     <h2 class="text-gray-600 text-3xl">Minhas Especialidades</h2>
 
     <ul class="list-disc text-gray-600 text-2xl mt-4">
@@ -40,7 +47,7 @@
     </ul>
   </div>
 
-  <div class="px-[200px] mt-24 flex flex-col justify-end items-end">
+  <div class="px-[200px] mt-24 flex flex-col justify-end items-end moveOn">
     <h2 class="text-gray-600 text-3xl mr-[9%]">Minhas Skills</h2>
 
     <ul class="list-disc text-gray-600 text-2xl mt-4">
@@ -55,4 +62,35 @@
 import gsap from 'gsap'
 </script>
 
-<style scoped></style>
+<style scoped>
+.gradient-circle {
+        background: linear-gradient(270deg, #42d392, #42d392, #647eff);
+    animation: gradientShift-14d3142e 3s ease-in-out infinite;
+    left: -84%;
+    top: -81%;
+    width: 3000px;
+    height: 3000px;
+}
+
+.moveOn {
+  animation: moveIn;
+  animation-duration: 1.5s;
+  animation-fill-mode: forwards;
+  animation-timing-function: ease-in-out;
+}
+
+@keyframes moveIn {
+  from {
+    transform: translateY(0px);
+  }
+}
+
+@keyframes gradientShift {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
